aa262d33450547f60757464a817b3c6dc1891de0
dpfls2405-oss <dpfls2405@gmail.com>
2026-03-13 15:01:41 +0900

    Add files via upload

--- 1_Upload.py
+++ 1_Upload.py
@@ -1,5 +1,9 @@
 from __future__ import annotations
 
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import streamlit as st
 import pandas as pd
 
